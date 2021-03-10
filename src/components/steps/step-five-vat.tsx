95f4fd12cf1c023c74e3d4401b0d3041996a2ffe
Abdulrahman Falyoun <abdulrahman-falyoun@outlook.com>
2021-03-10 15:43:03 +0200

    Merging doc & vat

--- src/components/steps/step-five-vat.tsx
+++ src/components/steps/step-five-vat.tsx
@@ -16,7 +16,8 @@ const StepFiveVAT = () => {
     const [form] = Form.useForm();
 
     const [taxCertificate, setTaxCertificate] = useState(null);
-
+    const [tradeLicense, setTradeLicense] = useState(null);
+    const [nationalId, setNationalId] = useState(null);
 
     return (
         <div className="step-five-wrapper">
@@ -24,6 +25,18 @@ const StepFiveVAT = () => {
             <div >
                 <div>
                     <Form {...layout} form={form} name="control-hooks">
+                        <div className="flex-row-flex-start-main-cross-center">
+                            <Form.Item className="same-width-flex-item column-flex-direction" name="tradeLicense" label="Upload Trade License">
+                                <StepperUploadFileInput id="trade-license-input" placeholder="Browse Files" onFileSelected={(file: any) => { console.log('tradeLicense: ', file); setTradeLicense(file); }} />
+                                {tradeLicense && <br />}
+                                {tradeLicense && <FancyCard title={getFileName(tradeLicense) + '  ' + getFileSize(tradeLicense)} width='100%' bgColor='#F9F9F9' txtStyle={{ color: 'black', fontSize: '.8rem', letterSpacing: '.1rem' }} borderStyle='dashed' borderWidth='2px' borderColor='#EAEAEA' />}
+                            </Form.Item>
+                            <Form.Item className="same-width-flex-item column-flex-direction" name="nationalId" label="Upload National ID(Saudi lqama Or Passport)">
+                                <StepperUploadFileInput id="national-id-input" placeholder="Browse Files" onFileSelected={(file: any) => { console.log('national: ', file); setNationalId(file); }} />
+                                {nationalId && <br />}
+                                {nationalId && <FancyCard title={getFileName(nationalId) + '  ' + getFileSize(nationalId)} width='100%' bgColor='#F9F9F9' txtStyle={{ color: 'black', fontSize: '.8rem', letterSpacing: '.1rem' }} borderStyle='dashed' borderWidth='2px' borderColor='#EAEAEA' />}
+                            </Form.Item>
+                        </div>
                         <div className="flex-row-flex-start-main-cross-center">
                             <StepperInput
                                 onInputChanged={(e: any) => { console.log('changed: ', e.target.value) }}

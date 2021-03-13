6aa20c049d6065392ee63dec379d912f0d99e39d
Abdulrahman Falyoun <abdulrahman-falyoun@outlook.com>
2021-03-13 15:52:13 +0200

    Add translations

--- src/components/card/card-buttons.tsx
+++ src/components/card/card-buttons.tsx
@@ -1,7 +1,6 @@
 
 import { Button } from 'antd';
-import { connect } from 'react-redux';
-import { ReduxStateInterface } from '../../interfaces/redux-state';
+import { useTranslation } from 'react-i18next';
 import '../../styles/card/card-buttons.sass';
 
 const CardButtons = ({
@@ -10,7 +9,7 @@ const CardButtons = ({
     onPressingBackButton,
     onPressingNextButton,
     submitting }: any) => {
-
+    const { t, i18n } = useTranslation('common');
     const justifyingAcoordingToCurrentStep = currentStep > 0 ? 'justify-content-space-between' : 'justify-content-center';
     return (
         <div className={"steps-action " + justifyingAcoordingToCurrentStep}>
@@ -18,13 +17,13 @@ const CardButtons = ({
                 currentStep > 0 && currentStep < totalSteps - 1
                 &&
                 <Button disabled={submitting} ghost={false} className="multi-step-form-button grey-button" style={{ marginLeft: 8 }} onClick={() => onPressingBackButton()}>
-                    Back
+                    {t('back')}
                 </Button>
             }
             {
                 currentStep < totalSteps - 1
                 &&
-                <Button disabled={submitting} className="multi-step-form-button yellow-button" size="large" onClick={() => onPressingNextButton()}>Next</Button>
+                <Button disabled={submitting} className="multi-step-form-button yellow-button" size="large" onClick={() => onPressingNextButton()}>{t('next')}</Button>
             }
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,33 @@ import reportWebVitals from './reportWebVitals';
 import SellerForm from './components/seller-form';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import common_en from "./translations/en.json";
+import common_ar from "./translations/ar.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },  // React already does escaping
+  lng: 'en',                              // language to use
+  resources: {
+    en: {
+      common: common_en               // 'common' is our custom namespace
+    },
+    ar: {
+      common: common_ar
+    },
+  },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <SellerForm />
-    </Provider>
+    <I18nextProvider i18n={i18next}>
+      <Provider store={store}>
+        <SellerForm />
+      </Provider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

a28027b313016d72ce5e837da7894315c14979f1
Abdulrahman Falyoun <abdulrahman-falyoun@outlook.com>
2021-03-18 10:15:43 +0200

    Adding translations for last step

--- src/components/steps/end-step.tsx
+++ src/components/steps/end-step.tsx
@@ -2,18 +2,20 @@
 import { FinalStepSVGIcon } from '../svg-icons';
 import React from 'react';
 import { Button } from 'antd';
+import { useTranslation } from 'react-i18next';
 const EndStep = () => {
+    const { t, i18n } = useTranslation('common');
     return (
         <div className="flex-column-center-main-cross">
             <div className="happy-man">
                 {FinalStepSVGIcon}
             </div>
             <div className="success-msg">
-                <p className="success-title-word">SUCCESS</p>
-                <p className="success-msg-body">You Have Been Successfully Sended</p>
+                <p className="success-title-word">{t('success')}</p>
+                <p className="success-msg-body">{t('you have been successfully registerd as seller')}</p>
             </div>
             <Button ghost={false} className="multi-step-form-button yellow-button" style={{ marginLeft: 8 }} onClick={() => { }}>
-                Done
+                {t('done')}
             </Button>
         </div>
     )
